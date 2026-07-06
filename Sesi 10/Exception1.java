ec4fcbbea2b4d0f38a0dbfeb7286fd4d57c78b6b
Sumap <sumap123@gmail.com>
2026-07-06 19:44:16 +0700

    update

---
+++ Sesi 10/Exception1.java
@@ -0,0 +1,15 @@
+public class Exception1 {
+
+    public static void main(String[] args) {
+
+        int a[] = new int[5];
+
+        try {
+            a[5] = 100;
+        } catch (Exception e) {
+            System.out.println("Terjadi pelanggaran memory");
+        }
+
+    }
+
+}
